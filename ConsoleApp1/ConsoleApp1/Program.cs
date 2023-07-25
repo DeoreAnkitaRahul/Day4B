ec27a381bc8d1b3da2e9e1ce3cbd63144961ad79
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-07-25 17:16:51 +0530

    [Ankita] Add : UC1-count a total number of duplicate elements

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayCount arrayCount = new ArrayCount();
+            arrayCount.MethodArrayCount();
+            Console.ReadLine();
+        }
+    }
+}
